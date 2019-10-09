9ec3d94f86b0aa63bd41b36dd782f4f1fa3c7b63
Francis Mendez <fmendez884@gmail.com>
2019-10-09 04:04:08 -0400

    adjusts squalls animation threshold

--- Assets/Scripts/Controllers/PlayerMotor.cs
+++ Assets/Scripts/Controllers/PlayerMotor.cs
@@ -13,6 +13,7 @@ public class PlayerMotor : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        Debug.Log("sup");
     }
 
     // Update is called once per frame
